805ce117fc417665b9aaebde64c9f15d8ab078be
pace4747 <pace4747@gmail.com>
2025-07-20 16:11:46 -0500

    Update game.py

--- game.py
+++ game.py
@@ -2,24 +2,31 @@ import pygame
 from pygame.locals import *
 from player import Player
 from world import World
+from constants import *
 
 class Game:
     def __init__(self, screen):
         self.screen = screen
+        self.virtual_screen = pygame.Surface((VIRTUAL_WIDTH, VIRTUAL_HEIGHT))
         self.world = World(100, 50)  # Small map
-        self.player = Player((0, 0))
-        self.camera_pos = [0, 0]  # Follow player
+        self.player = Player((VIRTUAL_WIDTH // 2, VIRTUAL_HEIGHT // 2))  # Start center
+        self.camera_pos = [0, 0]
         self.all_sprites = pygame.sprite.Group(self.player)
 
-    def update(self):
+    def update(self, dt):
         keys = pygame.key.get_pressed()
-        self.player.update_movement(keys)
+        self.player.update_movement(keys, dt)
         # Update camera to center player
-        self.camera_pos[0] = self.player.rect.x - (SCREEN_WIDTH // 2)
-        self.camera_pos[1] = self.player.rect.y - (SCREEN_HEIGHT // 2)
+        self.camera_pos[0] = self.player.rect.centerx - (VIRTUAL_WIDTH // 2)
+        self.camera_pos[1] = self.player.rect.centery - (VIRTUAL_HEIGHT // 2)
+        # Clamp camera
+        self.camera_pos[0] = max(0, min(self.camera_pos[0], self.world.width * TILE_SIZE - VIRTUAL_WIDTH))
+        self.camera_pos[1] = max(0, min(self.camera_pos[1], self.world.height * TILE_SIZE - VIRTUAL_HEIGHT))
 
     def draw(self):
-        self.screen.fill(COLORS['BLACK'])
-        self.world.draw(self.screen, self.camera_pos)
-        self.all_sprites.draw(self.screen)
+        self.virtual_screen.fill(COLORS['BLACK'])
+        self.world.draw(self.virtual_screen, self.camera_pos)
+        self.all_sprites.draw(self.virtual_screen)
+        scaled_surf = pygame.transform.scale(self.virtual_screen, (SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.screen.blit(scaled_surf, (0, 0))
         pygame.display.flip()
