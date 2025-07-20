a6cb75f6b24c0b5702e8ec1e50063321a77bc435
pace4747 <pace4747@gmail.com>
2025-07-20 16:12:19 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from constants import *
 from game import Game
 
 def main():
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("Snakes and Spiders Island")
     clock = pygame.time.Clock()
@@ -11,13 +12,13 @@ def main():
     running = True
 
     while running:
+        dt = clock.tick(FPS) / 1000.0  # Delta time in seconds
         for event in pygame.event.get():
             if event.type == QUIT:
                 running = False
 
-        game.update()
+        game.update(dt)
         game.draw()
-        clock.tick(FPS)
 
     pygame.quit()
 
